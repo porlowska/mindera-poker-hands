9d42ef2970b093e0180f4b7f4ed8a4a955758f14
Paulina Orłowska <polly@Paulinas-MBP.home>
2024-04-10 01:04:47 +0100

    added new code verison with use of functions

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function pokerHands(x){
+function pokerHands(x) {
     //create a deck of cards
     const suits = ["h", "d", "c", "s"]; // Hearts, Dimonds, CLubs, Spades
-    const ranks = [2,3,4,5,6,7,8,9,10,11,12,13,14]; // J = 11, Q = 12 K=13 A= 14/ 1
+    const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; // J = 11, Q = 12 K=13 A= 14/ 1
     const deck = [];
-        for (const suit of suits) {
-            for (const rank of ranks) {
-                deck.push([rank, suit]);
-            }
+    for (const suit of suits) {
+        for (const rank of ranks) {
+            deck.push([rank, suit]);
         }
+    }
 
     let straightFlush = 0 //5 same suit, seqential rank
     let flush = 0 //5 same suit, different numer
@@ -15,7 +15,7 @@ function pokerHands(x){
     let fullHouse = 0 //3 same number  and 2 same number, diferent suit 
     let threeOfAKind = 0 //3 same cards numbers, different suit 
     let straight = 0 // seqential rank, different suit 
-    
+
     console.log('Number of Traials: ' + x)
     // itterate through hands
     for (let i = x; i > 0; i--) {
@@ -27,89 +27,93 @@ function pokerHands(x){
             deck[j] = temp;
         }
         //draw 5 cards:
-        const hand = deck.slice(0,5)
+        const hand = deck.slice(0, 5)
         //sort scards in ascending order 
         hand.sort((a, b) => a[0] - b[0]);
-        
+
         // check hand
-            // ALL same suit, 
-            // checks if each card is equal to the first one 
-            if (hand.every(card => card[1] === hand[0][1])){
-                for (let i = 0; i < hand.length - 1; i++) {
-                    if (hand[i][0] === hand[i + 1][0] - 1) {
-                        straightFlush++;
-                    } else {
-                        flush++;
-                    }
-                }   
+        // ALL same suit, 
+        // checks if each card is equal to the first one 
+        if (hand.every(card => card[1] === hand[0][1])) {
+
+            for (let i = 0; i < hand.length - 1; i++) {
+                if (hand[i][0] === hand[i + 1][0] - 1) {
+                    // change loop as it only checks the first one 
+                    //function to check if it in sequence or not 
+                    //in sequence checking 
+                    straightFlush++;
+                } else {
+                    flush++;
+                }
+            }
             //Different suit
-            }else {
-                //all cards are already sorted so same numbers must be next to each other!
-                let isSequial;
-                for (let i = 0; i < hand.length - 1; i++) {
-                    if (hand[i][0] === hand[i + 1][0] - 1) {
-                        isSequial === true
+        } else {
+            //all cards are already sorted so same numbers must be next to each other!
+            let isSequial;
+            for (let i = 0; i < hand.length - 1; i++) {
+                if (hand[i][0] === hand[i + 1][0] - 1) {
+                    isSequial === true
+                } else {
+                    isSequial === false
+                }
+            }
+            if (isSequial) {
+                straight++
+            } else {
+                let pair = 0 // you should heve functon that will be called to check the hand and focus.
+                let trio = 0
+                if (hand[0][0] === hand[1][0]) {
+                    pair++
+                }
+                if (hand[1][0] === hand[2][0]) {
+                    if (pair == 0) {
+                        pair++
                     } else {
-                        isSequial === false
+                        pair--
+                        trio++
                     }
-                } 
-                if (isSequial){
-                    straight ++
-                } else{
-                    let pair = 0 
-                    let trio = 0 
-                    if (hand[0][0]===hand[1][0]){
-                        pair ++
+                }
+                if (hand[2][0] === hand[3][0]) {
+                    if (trio == 1) {
+                        fourOfAKind++
+                        trio--
+                    } else if (pair == 1) {
+                        pair--
+                        trio++
+                    } else {
+                        pair++
                     }
-                    if (hand[1][0] === hand[2][0]){
-                        if (pair==0){
-                            pair++
-                        }else {
-                            pair --
-                            trio ++
-                        }
+                }
+                if (hand[3][0] === hand[4][0]) {
+                    if (trio == 1) {
+                        fourOfAKind++
+                        trio--
+                    } else if (pair >= 1) {
+                        pair--
+                        trio++
                     }
-                    if (hand[2][0] === hand[3][0]){
-                        if(trio==1){
-                            fourOfAKind ++
-                            trio --
-                        }else if (pair ==1){
-                            pair --
-                            trio ++
-                        }else {
-                            pair ++
-                        }
-                    }
-                    if (hand[3][0] === hand[4][0]){
-                        if (trio==1){
-                            fourOfAKind++
-                            trio --
-                        } else if (pair>=1){
-                            pair --
-                            trio ++
-                        }
-                        
-                    }
-                    if (trio==1){
-                        if (pair==0){
-                            threeOfAKind ++
-                        }else{
-                            fullHouse++
-                        }
+
+                }
+                if (trio == 1) {
+                    if (pair == 0) {
+                        threeOfAKind++
+                    } else {
+                        fullHouse++
                     }
                 }
             }
-    } 
-     // Probablility :
-    const pStraightFlush = Math.round((straightFlush/x)*10000)/100
-    const pFlush = (flush/x)*100
-    const pFourOfAKind = (fourOfAKind/x)*100
-    const pFullHouse = (fullHouse/x)*100
-    const pThreeOfAKind = (threeOfAKind/x)*100
-    const pStraight = (straight/x)*100
-    const pWin = ((straight+flush+straightFlush+fourOfAKind+threeOfAKind+fullHouse)/x)*100
+        }
+    }
+    // Probablility :
+    const pStraightFlush = Math.round((straightFlush / x) * 10000) / 100
+    const pFlush = (flush / x) * 100
+    const pFourOfAKind = (fourOfAKind / x) * 100
+    const pFullHouse = (fullHouse / x) * 100
+    const pThreeOfAKind = (threeOfAKind / x) * 100
+    const pStraight = (straight / x) * 100
+    const pWin = ((straight + flush + straightFlush + fourOfAKind + threeOfAKind + fullHouse) / x) * 100
 
-    console.log( 
+    console.log(
         `Probablility of a hand with: \n 
         Straight Flush: ${pStraightFlush} % \n
         Flush: ${pFlush} \n
@@ -119,8 +123,14 @@ function pokerHands(x){
         Straight: ${pStraight} %\n \n 
         Probablilty of winning with any of the above: ${pWin} %`
     )
+
+    document.get
 }
 
 const NUM_TRIALS = prompt("Enter numnber fo trails, min 1, recommeended above 100")
 
 pokerHands(parseInt(NUM_TRIALS))
+
+//more functions
+//semicolons !!!
+//trio checking 
---
+++ index2.js
@@ -0,0 +1,163 @@
+//function which will start the couning
+const pokerHands = (x) =>{
+  let deck = createDeck();
+
+  let straightFlush = 0; //5 same suit, seqential rank
+  let flush = 0; //5 same suit, different numer
+  let fourOfAKind = 0; // 4 same number, different suit
+  let fullHouse = 0; //3 same number  and 2 same number, diferent suit
+  let threeOfAKind = 0; //3 same cards numbers, different suit
+  let straight = 0; // seqential rank, different suit
+
+  for (let i = 0; i < x; i++) {
+    let hand = drawCards(deck); // draw 5
+
+    let sameSuit = isSameSuit(hand);
+    let inSequence = isInSequence(hand);
+    let sameNumber = isSameNumber(hand);
+
+    if (sameSuit) {
+      if (inSequence) {
+        straightFlush++;
+      } else {
+        flush++;
+      }
+    } else {
+      if (inSequence) {
+        straight++;
+      } else if (sameNumber == "three") {
+        threeOfAKind++;
+      } else if (sameNumber == "four") {
+        fourOfAKind++;
+      } else if (sameNumber == "full") {
+        fullHouse++;
+      }
+    }
+  }
+  // Probablility :
+  calculateProbability(
+    straightFlush,
+    flush,
+    fourOfAKind,
+    fullHouse,
+    threeOfAKind,
+    straight,
+    x
+  );
+}
+
+// function to create the deck
+const createDeck = () => {
+  //create a deck of cards
+  const suits = ["h", "d", "c", "s"]; // Hearts, Dimonds, CLubs, Spades
+  const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; // J = 11, Q = 12 K=13 A= 14/ 1
+  const deck = [];
+  for (const suit of suits) {
+    for (const rank of ranks) {
+      deck.push([rank, suit]);
+    }
+  }
+  return deck;
+}
+
+// function to shuffle the deck, draw 5 cards and sort them out
+const drawCards = (deckArr) => {
+  //suffle
+  for (let i = deckArr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let temp = deckArr[i];
+    deckArr[i] = deckArr[j];
+    deckArr[j] = temp;
+  }
+  //draw 5 cards:
+  const hand = deckArr.slice(0, 5);
+  //sort scards in ascending order
+  hand.sort((a, b) => a[0] - b[0]);
+
+  return hand;
+}
+
+// function to check if same suit
+const isSameSuit = (handArr) =>{
+  if (handArr.every((card) => card[1] === handArr[0][1])) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function to check if in sequence
+function isInSequence(handArr) {
+  for (let i = 0; i < handArr.length - 1; i++) {
+    if (handArr[i + 1][0] - handArr[i][0] !== 1) {
+      return false; // Sequence broken
+    }
+  }
+  return true; // All elements are in sequence
+}
+
+// function check how many same numers 2 same 3 sam 4 same
+function isSameNumber(handArr) {
+  //extract card numbers(no) only
+
+  let no = handArr.map((card) => card[0]);
+  //console.log(no);
+
+  const winningHand = new Set(no);
+  //console.log(winningHand.size);
+
+  // if a set has 2 uniqe entries
+  //4same no (xxxx y) (y xxxx) 2222 4
+  //3 same no 2 same no (xxx yy)(yy xxx)
+
+  // XXXX Y
+  // XXX YY
+  // XX YYY
+  // X YYYY
+
+  if (winningHand.size == 2 && no[1] === no[3]) {
+    console.log("four");
+    return "four";
+  } else if (winningHand.size == 2 && no[1] !== no[3]) {
+    console.log("full");
+    return "full";
+    // if set has 3 uniqe entries
+    //3 same no (xxx yz)(y xxx z)(yz xxx)
+  } else if (
+    winningHand.size == 3 &&
+    (no[0] === no[2] || no[2] === no[4] || no[1] === no[3])
+  ) {
+    console.log("three");
+    return "three";
+  } else {
+    console.log("nope");
+  }
+}
+
+// function to calculate probability
+let probability = (n, y) => Math.round((n / y) * 100000) / 1000;
+
+// calculate probability
+function calculateProbability(a, b, c, d, e, f, x) {
+  let pSF = probability(a, x);
+  let pF = probability(b, x);
+  let p4K = probability(c, x);
+  let pFH = probability(d, x);
+  let p3K = probability(e, x);
+  let pS = probability(f, x);
+
+  console.log(
+    `Probablility of a hand with: \n 
+      Straight Flush: ${pSF} % \n
+      Flush: ${pF} % \n
+      Four of a Kind: ${p4K} % \n
+      Full House: ${pFH} %\n
+      Three of a Kind: ${p3K} %\n
+      Straight: ${pS} %`
+  );
+}
+
+const NUM_TRIALS = prompt(
+  "Enter numnber fo trails, min 1, recommeended above 100"
+);
+pokerHands(parseInt(NUM_TRIALS));
